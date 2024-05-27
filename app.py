40365199766271984cff55b73ed16393b676b120
Saikat98 <saikatsutradhar98@gmail.com>
2024-05-27 16:39:16 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -17,14 +17,8 @@ def upload():
 
     try:
         answer = get_answer(query=query,passage=passage)
-        # "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     except:
         answer = "Sorry. Try again after sometime"
-
-    # if query.lower() == "hi":
-    #     answer = "This fucking message has been recieved"
-    # else:
-    #     answer = passage
 
     return jsonify(
         {
